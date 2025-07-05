fd5128b849a03c7e923902c1fc18e728adc6a11c
Jose Luis Salas <josacar@users.noreply.github.com>
2025-07-05 20:40:23 +0200

    Update neovim plugins

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,11 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+local home_dir = os.getenv("HOME")
+
 ---@class ParserInfo[]
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.crystal = {
   install_info = {
-    url = "/home/selu/code/tree-sitter-crystal",
+    url = home_dir .. "/code/tree-sitter-crystal",
     files = { "src/parser.c", "src/scanner.c" },
     branch = "main",
   },
