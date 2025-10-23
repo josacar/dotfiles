a9ec359db0ef71495169d57ae13f1e16ba2aa2b2
Jose Luis Salas <josacar@users.noreply.github.com>
2025-10-23 22:15:27 +0200

    Use metal / vulkan acceleration in Wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,23 +1,26 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'iTerm2'
+-- Use vulkan / metal acceleration
+config.front_end = "WebGpu"
+
+config.color_scheme = "iTerm2"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.colors = {
-  foreground = 'white',
-  background = 'black',
+  foreground = "white",
+  background = "black",
 }
 
 -- config.color_scheme = 'Wez'
 
 -- config.font = wezterm.font('Menlo', { weight = 'Regular' })
-config.font = wezterm.font('JetBrains Mono', { weight = 'Regular', italic = false })
+config.font = wezterm.font("JetBrains Mono", { weight = "Regular", italic = false })
 config.font_size = 16.0
 
 -- and finally, return the configuration to wezterm
